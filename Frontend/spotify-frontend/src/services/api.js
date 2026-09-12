180bb1396902e3c63b16300fe2c4516356aeb329
abbasaman-says <abbaskhan462894162@gmail.com>
2026-09-12 16:35:04 +0500

    Update backend URL to Railway

--- Frontend/spotify-frontend/src/services/api.js
+++ Frontend/spotify-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://cooperative-elegance-production-07a5.up.railway.app",
     withCredentials: true
 });
 
